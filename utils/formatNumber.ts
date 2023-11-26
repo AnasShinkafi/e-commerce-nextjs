d980a24285ad672902c9378d415c3853ba882625
Anas Isah <anasisahskf59@gmail.com>
2023-11-26 18:06:52 +0100

    fix formatPrice

--- utils/formatNumber.ts
+++ utils/formatNumber.ts
@@ -2,4 +2,15 @@
 //     return new Intl.NumberFormat('en-US').format(digit);
 // };
 
-export const formatNumber = (digit: number) => new Intl.NumberFormat('en-US').format(digit);
+// export const formatNumber = (digit: number) => new Intl.NumberFormat('en-US').format(digit);
+
+
+
+export function formatNumber(digit:number) {
+    return new Intl.NumberFormat('en-US').format(digit);
+  }
+  
+  // Example usage:
+  const formattedNumber = formatNumber(1234567.89);
+  console.log(formattedNumber);  // Output: 1,234,567.89
+  
