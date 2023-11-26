c1f0d1d9fbafdc59a8f4f58a51825c8310054858
Anas Isah <anasisahskf59@gmail.com>
2023-11-26 17:24:07 +0100

    fix formatPrice

--- utils/formatNumber.ts
+++ utils/formatNumber.ts
@@ -1,3 +1,5 @@
-export const formatNumber = (digit: number) => {
-    return new Intl.NumberFormat('en-US').format(digit);
-};
+// export const formatNumber = (digit: number) => {
+//     return new Intl.NumberFormat('en-US').format(digit);
+// };
+
+export const formatNumber = (digit: number) => new Intl.NumberFormat('en-US').format(digit);
